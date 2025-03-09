ea7aaab44c28d1f7ff0055aba9a4b9ad1d27f19a
Pawelnougoed <46395722+Pawelnougoed@users.noreply.github.com>
2025-03-09 23:13:25 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,7 @@
+
+    import * as steaminventory from './index.js'
+    var steamid = '76561198089544929';
+    steaminventory.getinventory(730, steamid, '2').then(data => {
+        console.log(data.marketnames);
+    }).catch(err => console.log(err));
+
